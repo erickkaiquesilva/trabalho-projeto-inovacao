a67ea286435f7c0bbbd276dc854a539bd2524ae8
Aluno <Aluno@Digital.net>
2018-10-09 11:54:47 -0300

    Atualização Site Institucional

---
+++ public/js/scripts.js
@@ -0,0 +1,10 @@
+var target = $('.anime');
+var animationClass = 'anime-start';
+
+function animeScrool(){
+
+    var docTop = $(document).scrollTop();
+    console.log(docTop);
+}
+
+animeScrool();
